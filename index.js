805e21473613eab6b9487a764b98089502d25f71
VishnuVVR-369 <vishnuvardhanganji@gmail.com>
2022-02-28 00:53:41 +0530

    Added async function

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ require("dotenv").config();
 const app = express();
 
 let PORT = process.env.PORT || 3000;
+let mailOptions;
+
+async function sendMail() {
+  await transporter.sendMail(mailOptions, function (err, success) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Mail sent successfully");
+    }
+  });
+}
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -18,24 +29,18 @@ let transporter = nodemailer.createTransport({
   },
 });
 
-lineReader.eachLine("Mails.txt", (line, last) => {
+lineReader.eachLine("Test.txt", (line, last) => {
   // Mails.txt is a text file which contains names and mail ids in the form NAME : MAILID
   Mail = line.split(" : ");
 
-  let mailOptions = {
+  mailOptions = {
     from: process.env.MAIL_ID, // Your Mail ID
     to: Mail[1], // Receiers Mail ID
     subject: "Hello " + Mail[0], // Subject Line of your mail
     text: "This is a mail sent using automated nodejs program. Just ignore it.", // Body of your Mail ID
   };
 
-  transporter.sendMail(mailOptions, function (err, success) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Mail sent successfully");
-    }
-  });
+  sendMail();
 });
 
 app.get("/", (req, res) => {
